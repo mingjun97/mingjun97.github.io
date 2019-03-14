8e64e19d5a6e14d19b6b06661a7b0383ed3a484e
mingjun97 <470890875@qq.com>
2019-03-14 16:28:57 -0700

    Site updated: 2019-03-14 16:28:57

---
+++ watchfox/precache-manifest.0a1a525b5a8ec34b12275e1ad525fb89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5fc67f176b66dc95c1c0",
+    "url": "/static/js/main.a9998c6d.chunk.js"
+  },
+  {
+    "revision": "b77b31caf63d26144773",
+    "url": "/static/js/2.1cbadd71.chunk.js"
+  },
+  {
+    "revision": "5fc67f176b66dc95c1c0",
+    "url": "/static/css/main.64886a66.chunk.css"
+  },
+  {
+    "revision": "e33aaafa418e2197057cafba31a6a88f",
+    "url": "/index.html"
+  }
+];
